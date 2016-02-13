742ea80af4cd481bd8e938e45c3ca7e51b9929ec
yidahuliangh <yidahulian2@qq.com>
2016-02-14 03:24:10 +0800

    上传demo

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Mr Qian on 16/2/14.
+//  Copyright © 2016年 Mr Qian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
